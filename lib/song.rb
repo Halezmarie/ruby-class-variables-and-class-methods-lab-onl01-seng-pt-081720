a7f8c127c873737bbd4881724c4b5cad6c03ec10
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-16 02:12:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ def intialize(song_name,artist, genre)
   @genre = genre
   @@count += 1 
   @@artists << artist
-  
+  @@genres
 
 # We need our `Song` class to be able to show us all of the artists of existing songs:
 
