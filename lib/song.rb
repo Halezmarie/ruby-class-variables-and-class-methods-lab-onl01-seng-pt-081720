163f0717545904ec69c9cfea43e2596007140914
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-16 02:22:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,43 @@
 # In this lab, we'll be dealing with a `Song` class. The `Song` class can produce individual songs. Each song has a name, an artist and a genre. We need our `Song` class to be able to keep track of the number of songs that it creates.
-require 'pry'
+# require 'pry'
 
-class Song 
+# class Song 
   
-attr_accessor :name, :artist, :genre 
+# attr_accessor :name, :artist, :genre 
 
-@@count = 0 #lab says to set to 0 
-@@artists = []
-@@genres = []
+# @@count = 0 #lab says to set to 0 
+# @@artists = []
+# @@genres = []
 
-def initialize(song_name,artist, genre)
-  @name = song_name
-  @artist = artist 
-  @genre = genre
-  @@count += 1 
-  @@artists << artist
-  @@genres << genre 
-end 
+# def initialize(song_name,artist, genre)
+#   @name = song_name
+#   @artist = artist 
+#   @genre = genre
+#   @@count += 1 
+#   @@artists << artist
+#   @@genres << genre 
+# end 
 
-def self.count 
-  @@count
-end
+# def self.count 
+#   @@count
+# end
 
-def self.artists
-  @@artists.uniq 
-end 
+# def self.artists
+#   @@artists.uniq 
+# end 
 
-def self.genre_count
-  genre_count = Hash.new(0)
-  @@genres.each {|genre| genre_count[genre] += 1}
-  genre_count 
-end
+# def self.genre_count
+#   genre_count = Hash.new(0)
+#   @@genres.each {|genre| genre_count[genre] += 1}
+#   genre_count 
+# end
 
-def self.artist_count
-  artist_count = Hash.new(0)
-  @@artists.each {|artist| artist_count[artist] += 1}
-  artist_count
-end
-end 
+# def self.artist_count
+#   artist_count = Hash.new(0)
+#   @@artists.each {|artist| artist_count[artist] += 1}
+#   artist_count
+# end
+# end 
 
 
   
