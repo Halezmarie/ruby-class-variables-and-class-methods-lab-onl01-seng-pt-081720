4e62cc75b6bc397ae2decf0972297e002469795f
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-16 02:09:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ attr_accessor :name, :artist, :genre
 @@artists = []
 @@genres = []
 
-def intialize(song_name,)
+def intialize(song_name,artist, genre)
 
 # We need our `Song` class to be able to show us all of the artists of existing songs:
 
