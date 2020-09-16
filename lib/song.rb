c2e21fb8c8078c6e6adfb7085528caa5ae183565
Haley Grassi <teacherhaleymarie@gmail.com>
2020-09-16 02:04:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,7 @@
 # In this lab, we'll be dealing with a `Song` class. The `Song` class can produce individual songs. Each song has a name, an artist and a genre. We need our `Song` class to be able to keep track of the number of songs that it creates.
 
 
-def Song class(name, artist, genre)
-song.count => 30
-end 
+Class Song 
 
 # We need our `Song` class to be able to show us all of the artists of existing songs:
 
